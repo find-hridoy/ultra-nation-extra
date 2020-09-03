2bb592cc4a524e14ba11ba0107e973b11ac9c0b7
hridoy546 <mdhridoymia546@gmail.com>
2020-09-04 00:33:26 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button } from 'react-bootstrap';
+// import { Button } from '@material-ui/core';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+// import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button><FontAwesomeIcon icon={faCoffee} />Button</Button>
+      {/* <Button color="primary">Hello World</Button> material button and bootstrap button make jamela */}
+
     </div>
   );
 }
