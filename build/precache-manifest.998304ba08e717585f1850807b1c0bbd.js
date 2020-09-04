6e23a767c7e45f3ff40e5670c373144d483560bf
hridoy546 <mdhridoymia546@gmail.com>
2020-09-04 19:18:23 +0600

    project-completed

---
+++ build/precache-manifest.998304ba08e717585f1850807b1c0bbd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd1588196e114d3ed502d6b0830117b8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "baae9a2ddf24530003ad",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "3b3c0a896d5b5fb520db",
+    "url": "/static/css/main.f8ee5040.chunk.css"
+  },
+  {
+    "revision": "baae9a2ddf24530003ad",
+    "url": "/static/js/2.0e43bd40.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.0e43bd40.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b3c0a896d5b5fb520db",
+    "url": "/static/js/main.bf555fae.chunk.js"
+  },
+  {
+    "revision": "7a440a12f31bac17b2a2",
+    "url": "/static/js/runtime-main.5f113a13.js"
+  }
+]);
